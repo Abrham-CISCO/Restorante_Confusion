96c42fad5f2b99210c5e7d983fa2da4a322ac65e
Abrham-CISCO <58417725+Abrham-CISCO@users.noreply.github.com>
2022-10-12 10:03:29 +0300

    Update App.js

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Main from './components/MainComponent';
 import {View, Text} from 'react-native';
 
+//Just a comment
 export default class App extends React.Component {
   render() {
     return(
